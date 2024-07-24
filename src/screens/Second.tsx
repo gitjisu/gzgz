2d3923a822d143788f08768ffff258c0d865bfff
gitjisu <kimvely0422@gmail.com>
2024-07-24 23:09:07 +0900

    [JISU] feat : 인스타공유

--- src/screens/Second.tsx
+++ src/screens/Second.tsx
@@ -1,9 +1,17 @@
-import {View, Text, Pressable, Image, Modal} from 'react-native';
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  Modal,
+  Platform,
+  Dimensions,
+} from 'react-native';
 import React, {useRef, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AppNavigationType} from '../navigation/StackBase';
 import ViewShot from 'react-native-view-shot';
-
+import Share, {Social} from 'react-native-share';
 type Props = {
   navigation: AppNavigationType;
 };
@@ -11,8 +19,7 @@ type Props = {
 const Second = ({navigation}: Props) => {
   const imageRef = useRef<ViewShot>(null);
   const [shareImage, setShareImage] = useState<string | null>(null);
-  const handlePressShare = async () => {
-    console.log('자랑하기');
+  const handleSaveImage = async () => {
     // @ts-ignore
     const shareImage = await imageRef.current?.capture().then(res => {
       return res;
@@ -21,37 +28,62 @@ const Second = ({navigation}: Props) => {
       setShareImage(shareImage);
     }
   };
+
+  const handlePressShare = async () => {
+    // @ts-ignore
+    const shareImage = await imageRef.current?.capture().then(res => {
+      return res;
+    });
+    if (shareImage) {
+      const image =
+        Platform.OS === 'android' ? shareImage : `file://${shareImage}`;
+      console.log(image);
+      Share.shareSingle({
+        social: Share.Social.INSTAGRAM_STORIES as Social.InstagramStories, // 인스타그램 스토리로 공유
+        appId: 'facebook developer app id',
+        backgroundImage: image,
+        backgroundBottomColor: 'orange',
+        backgroundTopColor: 'orange',
+      });
+    }
+  };
   return (
-    <SafeAreaView>
-      <ViewShot ref={imageRef}>
-        <Image
-          style={{width: 300, height: 300}}
-          source={{
-            uri: 'https://velog.velcdn.com/images/aborile/post/2c73f647-268d-480d-a6f6-b0c914031621/image.png',
-          }}></Image>
-        <Text>인스타그램으로 꼬꼬!</Text>
-      </ViewShot>
-      <Pressable>
-        <Text>저장하기</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          handlePressShare();
-        }}>
-        <Text>자랑하기</Text>
-      </Pressable>
-      <Pressable onPress={() => navigation.goBack()}>
-        <Text>다시뽑기</Text>
-      </Pressable>
-      {shareImage && (
-        <View>
-          <Image
-            style={{width: 300, height: 300}}
-            source={{
-              uri: shareImage,
-            }}></Image>
-        </View>
-      )}
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 2, backgroundColor: 'red'}}>
+        <ViewShot ref={imageRef}>
+          <View>
+            <Image
+              resizeMode="contain"
+              style={{width: '100%', height: '100%'}}
+              source={{
+                uri: 'https://velog.velcdn.com/images/aborile/post/2c73f647-268d-480d-a6f6-b0c914031621/image.png',
+              }}></Image>
+
+            <Text style={{color: 'red'}}>인스타그램으로 꼬꼬!</Text>
+          </View>
+        </ViewShot>
+      </View>
+      <View style={{flex: 1, backgroundColor: 'orange'}}>
+        <Pressable
+          style={{flex: 1, borderWidth: 1}}
+          onPress={() => {
+            handleSaveImage();
+          }}>
+          <Text>저장하기</Text>
+        </Pressable>
+        <Pressable
+          style={{flex: 1, borderWidth: 1}}
+          onPress={() => {
+            handlePressShare();
+          }}>
+          <Text>자랑하기</Text>
+        </Pressable>
+        <Pressable
+          style={{flex: 1, borderWidth: 1}}
+          onPress={() => navigation.goBack()}>
+          <Text>다시뽑기</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
