bbb770292093a2aa5a54ad3bdd95f68506b1cc90
gitjisu <kimvely0422@gmail.com>
2024-07-30 19:00:21 +0900

    [JISU]

--- src/screens/Second.tsx
+++ src/screens/Second.tsx
@@ -6,12 +6,15 @@ import {
   Modal,
   Platform,
   Dimensions,
+  Alert,
 } from 'react-native';
 import React, {useRef, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AppNavigationType} from '../navigation/StackBase';
 import ViewShot from 'react-native-view-shot';
 import Share, {Social} from 'react-native-share';
+import {CameraRoll} from '@react-native-camera-roll/camera-roll';
+import RNFS from 'react-native-fs';
 type Props = {
   navigation: AppNavigationType;
 };
@@ -19,13 +22,48 @@ type Props = {
 const Second = ({navigation}: Props) => {
   const imageRef = useRef<ViewShot>(null);
   const [shareImage, setShareImage] = useState<string | null>(null);
+
   const handleSaveImage = async () => {
     // @ts-ignore
     const shareImage = await imageRef.current?.capture().then(res => {
       return res;
     });
     if (shareImage) {
-      setShareImage(shareImage);
+      const image =
+        Platform.OS === 'android' ? shareImage : `file://${shareImage}`;
+      const folderPath =
+        Platform.OS === 'ios'
+          ? `${RNFS.TemporaryDirectoryPath}/gzgz`
+          : `${RNFS.DownloadDirectoryPath}/gzgz`;
+      await RNFS.mkdir(folderPath, {NSURLIsExcludedFromBackupKey: true});
+      const source = image;
+      const fileType = source.split('.').pop();
+      const path = `${folderPath}/${`image-${new Date().getTime()}.${fileType}`}`;
+      try {
+        const result = await RNFS.downloadFile({
+          fromUrl: source,
+          toFile: path,
+        }).promise;
+
+        if (result.statusCode === 200) {
+          const iosResult = await CameraRoll.save(path);
+        }
+        // if (result) {
+        //   if (Platform.OS === 'android') {
+        //     RNFS.scanFile(path);
+        //     Alert.alert('success', '사진이 저장되었습니다.');
+        //   } else {
+        //     const iosResult = await CameraRoll.save(path, {type: 'photo'});
+
+        //     if (iosResult) {
+        //       Alert.alert('success', '사진이 저장되었습니다.');
+        //     }
+        //   }
+        // }
+      } catch (error) {
+        console.log(error);
+        return;
+      }
     }
   };
 
@@ -37,7 +75,6 @@ const Second = ({navigation}: Props) => {
     if (shareImage) {
       const image =
         Platform.OS === 'android' ? shareImage : `file://${shareImage}`;
-      console.log(image);
       Share.shareSingle({
         social: Share.Social.INSTAGRAM_STORIES as Social.InstagramStories, // 인스타그램 스토리로 공유
         appId: 'facebook developer app id',
